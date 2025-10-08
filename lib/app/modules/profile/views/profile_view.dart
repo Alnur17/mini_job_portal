91a8b468242186142ce157b3a63e439a1b1ecb37
Sultan Md. Alnur <98782302+Alnur17@users.noreply.github.com>
2025-10-08 17:47:15 +0600

    profile has been done

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:mini_job_portal/app/modules/saved_jobs/controllers/saved_jobs_controller.dart';
 import 'package:mini_job_portal/common/app_color/app_colors.dart';
+import 'package:mini_job_portal/common/app_text_style/styles.dart';
 
 import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
-  const ProfileView({super.key});
+  ProfileView({super.key});
+
+  final SavedJobsController savedJobsController = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.white,
       appBar: AppBar(
-        title: const Text('Profile'),
+        backgroundColor: AppColors.white,
+        scrolledUnderElevation: 0,
+        title: Text(
+          'Profile',
+          style: appBarStyle,
+        ),
         automaticallyImplyLeading: false,
       ),
       body: Column(
         children: [
           ListTile(
-            title: Text('Sultan'),
-            subtitle: Text("sultanmdalnur@gmail.com"),
+            title: Text(
+              'Sultan',
+              style: h3,
+            ),
+            subtitle: Text(
+              "sultanmdalnur@gmail.com",
+              style: h5,
+            ),
           ),
           Container(
-            padding: EdgeInsets.all(8),
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
             margin: EdgeInsets.symmetric(horizontal: 16),
             decoration: BoxDecoration(
               color: AppColors.silver,
@@ -31,8 +48,23 @@ class ProfileView extends GetView<ProfileController> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Number Saved Jobs'),
-                Text('10'),
+                Text(
+                  'Number Saved Jobs',
+                  style: h5,
+                ),
+                Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Center(
+                      child: Text(
+                        savedJobsController.savedJobs.length.toString(),
+                        style: h5,
+                      ),
+                    )),
               ],
             ),
           )
